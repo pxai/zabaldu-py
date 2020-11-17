a36ad6f27ee69e0bbd754f3fdda3c391306c4c22
pxai <pello.altadill@gluckgames.com>
2020-11-17 17:24:05 +0100

    Separated envs moved file

--- zabaldu/settings.py
+++ zabaldu/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
