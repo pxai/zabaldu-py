c836c3765421382caee2d7596471290c16aa0fce
pxai <pello.altadill@gluckgames.com>
2020-11-18 17:41:54 +0100

    Env aldagaiak funtzionatzen

--- zabaldu/settings.py
+++ zabaldu/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'zabaldu.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
 }
 
 
